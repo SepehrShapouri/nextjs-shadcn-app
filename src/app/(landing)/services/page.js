7d0cf70939aff5a88bdca45c29eac1d6d545ad92
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-05-10 21:26:27 +0330

    feat:new features added

--- src/app/(landing)/services/page.js
+++ src/app/(landing)/services/page.js
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
-
+import pay from "../../../../public/images/pay.png"
+import submit from "../../../../public/images/submit.png"
+import report from "../../../../public/images/report.png"
 function page() {
   return (
-    <div>page</div>
+    <div>
+        
+    </div>
   )
 }
 
